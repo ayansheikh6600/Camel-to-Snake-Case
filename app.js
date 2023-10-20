755c307f003d0e49238113829b196b1cc05c8d44
ayansheikh6600 <ayansheikh6600@gmail.com>
2023-10-21 00:11:48 +0500

    Done

--- app.js
+++ app.js
@@ -1,40 +1,65 @@
-const CameltoSanke = (name) => {
-    if(!name){
-      return "Please CamelCase String"
-    }
-    const temArry = [];
-    const test = name;
-    for (var i = 0; i < test.length; i++) {
-      if (test[i]==" "){
-   return "Please String Without Space and only CamelCase"
-      }
-      // console.log(test[i])
-      if (test[i] == test[i].toUpperCase()) {
+const CameltoSanke = (Obj) => {
+  //
+
+  // const testObj = {
+  //   FullName: "Ayan Sheikh",
+  //   DateOfBirth: 33,
+  //   Email: "sssss",
+  // };
+
+  let finalObj = {};
+
+  for (const key in Obj) {
+    if (Obj.hasOwnProperty(key)) {
+      // console.log(`${key}:`);
+
+      const temArry = [];
+      const test = key;
+      for (var i = 0; i < test.length; i++) {
+        if (test[i] == " ") {
+          return "Please String Without Space and only CamelCase";
+        }
         // console.log(test[i])
-        // console.log(i)
-        // const final = test[i]
-        // return;
-        temArry.push("_");
-        temArry.push(test[i]);
-      } else {
-        temArry.push(test[i].toLocaleLowerCase());
+        if (test[i] == test[i].toUpperCase()) {
+          // console.log(test[i])
+          // console.log(i)
+          // const final = test[i]
+          // return;
+          temArry.push("_");
+          temArry.push(test[i].toLocaleLowerCase());
+        } else {
+          temArry.push(test[i].toLocaleLowerCase());
+        }
+        // temArry.push(test[i].toLocaleLowerCase())
+        // isupper(test[i])
       }
-      // temArry.push(test[i].toLocaleLowerCase())
-      // isupper(test[i])
+
+      // console.log(temArry.join(""))
+      const done = temArry.join("");
+      const array = [];
+
+      for (var i = 1; i < done.length; i++) {
+        // console.log(done[i])
+        array.push(done[i]);
+      }
+      // console.log(array.join(""));
+
+      const updatedKey = array.join("");
+
+      finalObj[updatedKey] = Obj[key];
+      // console.log(finalObj)
     }
-  
-    // console.log(temArry.join(""))
-    const done = temArry.join("");
-    const array = [];
-  
-    for (var i = 1; i < done.length; i++) {
-      // console.log(done[i])
-      array.push(done[i]);
-    }
-    // console.log(array.join(""))
-    return array.join("");
-  };
-  
-  console.log(CameltoSanke("AyanSheikh"));
-  
+  }
+  // console.log(finalObj);
+  return (finalObj)
+};
+
+// const testObj = {
+//   FullName: "Ayan Sheikh",
+//   DateOfBirth: 33,
+//   Email: "sssss",
+// };
+
+// console.log(CameltoSanke(testObj))
+
   module.exports = CameltoSanke
